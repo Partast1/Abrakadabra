397d6996f234daca99e20372ce10c5717a0ee52d
Partast1 <andersa1989@gmail.com>
2021-04-09 16:09:49 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main( void) { /* abrakadabra.c */
+  char s[]= "abrakadabra";  /* virker */
+  /* char *s= "abrakadabra"; */ /* virker IKKE */
+  char *p;
+
+  printf("%s\n", s);
+  p = s;
+  while (*p != '\0') {
+        /**< Skifter bogstavet a ud med i */
+    if (*p == 'a') {
+      *p= 'i';
+    }
+    p++;
+  }
+
+  printf("%s\n", s);
+  return 0;
+}
